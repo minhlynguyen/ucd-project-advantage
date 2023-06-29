5117108839908f67d23aa5511a94397009653a04
Crane <95408805+CraneWvs@users.noreply.github.com>
2023-06-29 15:04:53 +0100

    basic structure for the routes

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import HomePage from './components/HomePage';
+// import LoginPage from './components/LoginPage';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <MyRoutes />
+    </Router>
+  );
 }
 
-export default App
+function MyRoutes() {
+  let location = useLocation();
+
+  return (
+    <div>
+      {/* Only show Header when not on LoginPage */}
+      {location.pathname !== '/login' && <Header />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/about" element={<AboutPage />} />
+        <Route path="/login" element={<LoginPage />} /> */}
+      </Routes>
+      {/* Only show Footer when not on LoginPage */}
+      {location.pathname !== '/login' && <Footer />}
+    </div>
+  );
+}
+
+
+export default App;
---
+++ react-app/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+export default function Header() {
+    return (
+        <nav>
+            <h1>Header</h1>
+        </nav>
+    )
+}
