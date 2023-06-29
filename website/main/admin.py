c9ab59c4a42b6b6059bcc3213f94de8fe8fde5ea
minhlynguyen <90552104+minhlynguyen@users.noreply.github.com>
2023-06-29 19:43:06 +0100

    Django DB data population across schemas

--- website/main/admin.py
+++ website/main/admin.py
@@ -2,5 +2,7 @@ from django.contrib.gis import admin
 
 # Register your models here.
 from .models import TaxiZone
+# from .models import Zone
 
 admin.site.register(TaxiZone, admin.ModelAdmin)
+# admin.site.register(Zone, admin.ModelAdmin)
--- website/main/models.py
+++ website/main/models.py
@@ -11,27 +11,47 @@ class Advertiser(models.Model):
 # class BillboardVendor(models.Model):
 #     user=models.ForeignKey(User,on_delete=models.CASCADE)
 
-# Taxi zone models
+# Taxi zone models in main DB
 from django.contrib.gis.db import models as geomodels
 from django.contrib.gis.geos import GEOSGeometry
 import json
 
+## Create table
 class TaxiZone(geomodels.Model):
     id = geomodels.PositiveIntegerField(primary_key=True)
     name = geomodels.CharField(max_length=45)
+    borough = geomodels.CharField(max_length=13)
     geom = geomodels.MultiPolygonField()
-    
-with open('../data-ingestions/nyc-taxi-zones.geojson', 'r') as fd:
-    data = json.load(fd)
 
-    for feature in data['features']:
-        geom = GEOSGeometry(str(feature['geometry']))
-        geonameid = feature['properties']['objectid']
-        geoname = feature['properties']['zone']
-        zone = TaxiZone(
-            id=geonameid,
-            geom=GEOSGeometry(geom),
-            name=geoname)
-        zone.save()
+## Populate data
+def taxi_zone_populate(model):
+    with open('../data-ingestions/nyc-taxi-zones.geojson', 'r') as fd:    
+        data = json.load(fd)
+        for feature in data['features']:
+            geom = GEOSGeometry(str(feature['geometry']))
+            geonameid = feature['properties']['objectid']
+            geoname = feature['properties']['zone']
+            geoborough = feature['properties']['borough']
+            zone = model(
+                id=geonameid,
+                geom=GEOSGeometry(geom),
+                name=geoname,
+                borough=geoborough)
+            zone.save()
 
-# class Place()
+taxi_zone_populate(TaxiZone)
+
+# Taxi zone models in maps schema
+
+## Create table
+class Zone(models.Model):
+    id = geomodels.PositiveIntegerField(primary_key=True)
+    name = geomodels.CharField(max_length=45)
+    borough = geomodels.CharField(max_length=13)
+    geom = geomodels.MultiPolygonField()
+    class Meta:
+        managed = True
+        db_table = 'maps\".\"zone'
+
+## Populate
+taxi_zone_populate(Zone)
