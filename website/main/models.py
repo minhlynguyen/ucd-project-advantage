584ebb3d90e3bf4f0c2bc205776e9da1373e8926
minhlynguyen <90552104+minhlynguyen@users.noreply.github.com>
2023-06-30 12:39:34 +0100

    Update instruction of .env

--- website/main/models.py
+++ website/main/models.py
@@ -14,30 +14,6 @@ class Advertiser(models.Model):
 # class BillboardVendor(models.Model):
 #     user=models.ForeignKey(User,on_delete=models.CASCADE)
 
-# Taxi zone models in public schema
-## Create table
-# class TaxiZone(geomodels.Model):
-#     id = geomodels.PositiveIntegerField(primary_key=True)
-#     name = geomodels.CharField(max_length=45)
-#     borough = geomodels.CharField(max_length=13)
-#     geom = geomodels.MultiPolygonField()
-## Populate data
-
-# def taxi_zone_populate(model):
-#     with open('../data-ingestions/nyc-taxi-zones.geojson', 'r') as fd:    
-#         data = json.load(fd)
-#         for feature in data['features']:
-#             geom = GEOSGeometry(str(feature['geometry']))
-#             geonameid = feature['properties']['objectid']
-#             geoname = feature['properties']['zone']
-#             geoborough = feature['properties']['borough']
-#             zone = model(
-#                 id=geonameid,
-#                 geom=GEOSGeometry(geom),
-#                 name=geoname,
-#                 borough=geoborough)
-#             zone.save()
-
 # Taxi zone models in maps schema
 ## Create table
 class Zone(models.Model):
@@ -48,9 +24,6 @@ class Zone(models.Model):
     class Meta:
         managed = True
         db_table = 'maps\".\"zone'
-
-## Populate
-# taxi_zone_populate(Zone)
 
 # PUMA models in maps schema
 class Puma(models.Model):
