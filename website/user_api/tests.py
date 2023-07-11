56faad9774aff45570b633d112af3033b5ef4ded
minhlynguyen <90552104+minhlynguyen@users.noreply.github.com>
2023-07-11 15:26:48 +0100

    Authentication added

--- website/user_api/tests.py
+++ website/user_api/tests.py
@@ -24,20 +24,3 @@ class UsersManagersTests(TestCase):
             User.objects.create_user(email="")
         with self.assertRaises(ValueError):
             User.objects.create_user(email="", password="foo")
-
-    def test_create_superuser(self):
-        User = get_user_model()
-        admin_user = User.objects.create_superuser(email="super@user.com", password="foo")
-        self.assertEqual(admin_user.email, "super@user.com")
-        # self.assertTrue(admin_user.is_active)
-        # self.assertTrue(admin_user.is_staff)
-        self.assertTrue(admin_user.is_superuser)
-        try:
-            # username is None for the AbstractUser option
-            # username does not exist for the AbstractBaseUser option
-            self.assertIsNone(admin_user.username)
-        except AttributeError:
-            pass
-        with self.assertRaises(ValueError):
-            User.objects.create_superuser(
-                email="super@user.com", password="foo", is_superuser=False)
--- website/website/settings.py
+++ website/website/settings.py
@@ -57,7 +57,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'website.urls'
