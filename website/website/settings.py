855fba80643ab5e2bd056ff5b4a71abbfb616daa
minhlynguyen <90552104+minhlynguyen@users.noreply.github.com>
2023-07-12 14:09:11 +0100

    Authentication add

--- website/website/settings.py
+++ website/website/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'main',
     'django.contrib.gis',
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
