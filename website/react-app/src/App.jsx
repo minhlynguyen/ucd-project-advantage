0a9ddc9899fe2d4d9f084cb063e6133ea8895144
CraneWvs <yiyun_uibe@126.com>
2023-07-12 04:30:22 +0100

    get data from /zones and fix the bug of duplicate axios requests

--- website/react-app/src/App.jsx
+++ website/react-app/src/App.jsx
@@ -27,7 +27,9 @@ function App() {
     <UserContext.Provider value={{ currentUser, setCurrentUser }}>
 
     <Router>
+      <HeaderContainer />
       <MyRoutes />
+      <Footer />
     </Router>
     </UserContext.Provider>
 
@@ -50,19 +52,48 @@ function MyRoutes() {
       )}
       <Routes>
         <Route path="/" element={<HomePage />} />
-
         <Route path='/solutions' element={<HeatmapPage />} />
-
         <Route path="/login" element={<LoginPage />} />
-
         <Route path="/signup" element={<SignupPage/>}/>
       </Routes>
-      
-      {/* Only show Footer when not on LoginPage */}
-      {location.pathname !== '/login' && <Footer />}
     </div>
   );
 }
+
+
+
+// function App() {
+//   return (
+//     <Router>
+//       <MyRoutes />
+//     </Router>
+//   );
+// }
+
+
+// function MyRoutes() {
+//   let location = useLocation();
+
+//   return (
+//     <div>
+//       {/* Only show Header when not on LoginPage */}
+//       {location.pathname !== '/login' && location.pathname !== '/signup' && <Header />}
+
+//       <Routes>
+//         <Route path="/" element={<HomePage />} />
+
+//         <Route path='/solutions' element={<HeatmapPage />} />
+
+//         <Route path="/login" element={<LoginPage />} />
+
+//         <Route path="/signup" element={<SignupPage/>}/>
+//       </Routes>
+      
+//       {/* Only show Footer when not on LoginPage */}
+//       {location.pathname !== '/login' && <Footer />}
+//     </div>
+//   );
+// }
 // import './App.css';
 // import ZoneComponent from  './components/BackendAPI/ZoneComponent'
 // function App() {
--- website/react-app/src/components/BackendAPI/ZoneComponent.jsx
+++ website/react-app/src/components/BackendAPI/ZoneComponent.jsx
@@ -1,32 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
-// axios.defaults.withCredentials = true;
-function ZoneComponent() {
-    const [zoneList, setZoneList] = useState([]);
-    useEffect(() => {
-        fetchData();
-    }, []);
-    // const apiURL = "http://127.0.0.1:8000/api/zones/";
-    const apiURL = "./NYC Taxi Zones.geojson";
-    const fetchData = async () => {
-        const response = await axios.get(apiURL
-            // ,
-            // {'withCredentials': true }
-            );
-        console.log(response)
-        setZoneList(response.data);
-        console.log(zoneList);
-        console.log(response.data);
-    }
-    return (
-        <div className="main-section">
-            <h1>All Zones</h1>
-            <div className="zone-list">
-                <ul>
-                    <li>{zoneList}</li>
-                </ul>
-            </div>
-        </div>
-    );
-}
-export default ZoneComponent;
+// import React, { useEffect, useState } from 'react'
+// import axios from 'axios';
+// // axios.defaults.withCredentials = true;
+// function ZoneComponent() {
+//     const [zoneList, setZoneList] = useState([]);
+//     useEffect(() => {
+//         fetchData();
+//     }, []);
+//     const apiURL = "http://127.0.0.1:8000/api/zones/";
+//     // const apiURL = "./NYC Taxi Zones.geojson";
+//     const fetchData = async () => {
+//         const response = await axios.get(apiURL
+//             // ,
+//             // {'withCredentials': true }
+//             );
+//         console.log(response)
+//         setZoneList(response.data);
+//         console.log(zoneList);
+//         console.log(response.data);
+//     }
+//     return (
+//         <div className="main-section">
+//             <h1>All Zones</h1>
+//             <div className="zone-list">
+//                 <ul>
+//                     <li>{zoneList}</li>
+//                 </ul>
+//             </div>
+//         </div>
+//     );
+// }
+// export default ZoneComponent;
