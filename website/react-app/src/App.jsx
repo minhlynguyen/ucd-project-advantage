865cb416346952c19cb5bf4842f273fac843471f
winnie imafidon <88711013+Winnie101995@users.noreply.github.com>
2023-07-13 16:42:08 +0100

    new sign in page and log in page styling

--- website/react-app/src/App.jsx
+++ website/react-app/src/App.jsx
@@ -1,29 +1,53 @@
 
 // eslint-disable-next-line no-unused-vars 
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import React, { createContext, useState, useContext } from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation} from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
 import LoginPage from './components/LoginPage/LoginPage';
 import SignupPage from './components/SignupPage/SignupPage';
 import Header from './components/Header/Header';
+import SignedInHeader from './components/Header/SignedInHeader/SignedInHeader'
 import Footer from './components/Footer/Footer';
 import HeatmapPage from './components/HeatmapPage/HeatmapPage';
 
+// Create a user context
+const UserContext = createContext();
+
+// const mockUser = {
+//   name: 'John Doe',
+//   email: 'johndoe@example.com'
+// };
+
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  // testing user log in 
+  // const [currentUser, setCurrentUser] = useState(mockUser);
+
   return (
+    <UserContext.Provider value={{ currentUser, setCurrentUser }}>
+
     <Router>
       <MyRoutes />
     </Router>
+    </UserContext.Provider>
+
   );
 }
 
 function MyRoutes() {
+  const { currentUser } = useContext(UserContext);
   let location = useLocation();
 
   return (
     <div>
       {/* Only show Header when not on LoginPage */}
-      {location.pathname !== '/login' && location.pathname !== '/signup' && <Header />}
+      {/* {location.pathname !== '/login' && location.pathname !== '/signup' && <Header />} */}
+      {/* Only show Header or SignedInHeader based on user login status */}
+      {location.pathname !== '/login' && location.pathname !== '/signup' && (
+
+          currentUser ? <SignedInHeader /> : <Header />
+
+      )}
       <Routes>
         <Route path="/" element={<HomePage />} />
 
@@ -51,3 +75,4 @@ function MyRoutes() {
 // }
 
 export default App;
+export {UserContext}
