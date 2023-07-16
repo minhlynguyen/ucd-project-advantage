7b74a7dbe0a5d91a964cdbcd63119f94e5d6d034
CraneWvs <yiyun_uibe@126.com>
2023-07-16 16:41:29 +0100

    modify time control to have two states, realtime and ad time

--- website/react-app/src/components/Solutions/DateTimeSelect.jsx
+++ website/react-app/src/components/Solutions/DateTimeSelect.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import {React, useState} from 'react';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
@@ -6,23 +6,51 @@ import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import { Typography } from '@mui/material';
 
 export default function DateTimeSelect() {
+  const [dateTimeStart, setDateTimeStart] = useState('');
+  const [dateTimeEnd, setDateTimeEnd] = useState('');
+  
+  const style = {
+    border: '1px solid #ccc', 
+    borderRadius: '10px', 
+
+  }
     
   return (
     <div>
 
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
+        {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
         <DateTimePicker
             label="Start DateTime"
             slotProps={{ textField: { size: 'small' } }}
         />
-        </LocalizationProvider>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
+        </LocalizationProvider> */}
+        {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DateTimePicker
                 label="End DateTime"
                 slotProps={{ textField: { size: 'small' } }}
             />
-        </LocalizationProvider>
-
+        </LocalizationProvider> */}
+            {/* <input
+              type="datetime-local"
+              value={dateTimeStart}
+              onChange={(e) => setDateTimeStart(e.target.value)}
+              className="map-form-input"
+            /> */}
+            <input
+              type="datetime-local"
+              value={dateTimeStart}
+              onChange={(e) => setDateTimeStart(e.target.value)}
+              className="map-form-input"
+              style={style}
+            />
+            <span> - </span>
+            <input
+              type="datetime-local"
+              value={dateTimeEnd}
+              onChange={(e) => setDateTimeEnd(e.target.value)}
+              className="map-form-input"
+              style={style}
+            />
 
     </div>
   );
