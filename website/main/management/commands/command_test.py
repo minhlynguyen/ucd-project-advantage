abff19b5c918c318326f2d71363dd86ea097cdd9
minhlynguyen <90552104+minhlynguyen@users.noreply.github.com>
2023-07-28 18:39:25 +0100

    Update schedule places

--- website/main/management/commands/command_test.py
+++ website/main/management/commands/command_test.py
@@ -1,6 +1,6 @@
 from django.utils import timezone
 import datetime
-from main.models import Zone, ZoneDetail
+from main.models import Zone, ZoneDetail, Puma, ZonePuma
 from django.contrib.gis.geos import Point 
 from zoneinfo import ZoneInfo
 from main.serializers import ZoneDataSerializer
@@ -19,58 +19,29 @@ from sklearn import metrics
 from sklearn.tree import export_graphviz
 import xgboost as xgb
 from xgboost import XGBRegressor
-import time
+import csv as csv
 
 
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
 
-    def handle(self, *args, **kwargs):
-
-        help = 'Aggregate the number of places for zone_detail'
-
-        # # Rating.objects.values('location_id').filter(attribute__in=attributes).annotate(sum_score=Sum('score')).order_by('-score')
-        # # https://stackoverflow.com/questions/13403609/how-to-group-by-and-aggregate-with-django
-
-        # test loading the model: DONE
-        loaded_model = pickle.load(open('../data-analytics/model_2/basic_XGboost_model_2.2.pkl', 'rb'))
-        # print(loaded_model)
-
-        # get the data from database
-        # zone = ZoneDetail.objects.filter(impression_predict__isnull=True)
-        zone = ZoneDetail.objects.all()
-        df = pd.DataFrame(list(zone.values('entertainment_and_recreation', 'financial_services', 
-                                           'food_and_beverage', 'parking_and_automotive_services',
-                                           'professional_services', 'real_estate', 'retail_services', 
-                                           'transportation', 'hospital', 'hotspots', 'school',
-                                           'total')))
-        print(df.head(5))
-
-
-
-
-
-
-
-
-
-        # def get_dummy_variable(data, categories):
-        #     dummy_dict = {}
-        #     for category in categories:
-        #         dummy_list = [1 if item == category else 0 for item in data]
-        #         dummy_dict[category] = dummy_list
-        #     return dummy_dict
-
-        #     # Example usage:
-        # data = ['entertainment_and_recreation','real_estate']
-        # categories = ['entertainment_and_recreation', 'financial_services', 'food_and_beverage', 'parking_and_automotive_services',
-        #                 'professional_services', 'real_estate', 'retail_services', 'transportation', 'hospital', 'hotspots', 'school']
-        # dummy_variables = get_dummy_variable(data, categories)
-
-        # for category, dummy_list in dummy_variables.items():
-            # print(f"{category}: {dummy_list}")
-
-        
-        # predictions = loaded_model.predict(input)
-        # print(predictions)
+    def handle(self,*args, **kwargs):
+        zones = Zone.objects.all()
+        pumas = Puma.objects.all()
+        for zone in zones:
+            zone_geom = zone.geom
+            for puma in pumas:
+                puma_geom = puma.geom
+                if zone_geom.intersects(puma_geom): 
+                    try: 
+                        intersection=zone_geom.intersection(puma_geom)   
+                        zonepuma = ZonePuma(
+                            zone = zone,
+                            puma = puma,
+                            intersection = intersection.area
+                        )
+                        zonepuma.save()
+                        print("Interection of zone", zone.id, "and puma", puma.id, "is created")
+                    except Exception as e:
+                        print(e)
