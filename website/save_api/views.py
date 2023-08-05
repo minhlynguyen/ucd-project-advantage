ffcaedf5b653386a62212a704a52b7e7b62afe8d
minhlynguyen <90552104+minhlynguyen@users.noreply.github.com>
2023-08-05 23:13:47 +0100

    Add error handle

--- website/save_api/views.py
+++ website/save_api/views.py
@@ -88,38 +88,38 @@ def saved_zone_info(request, zone):
     Retrieve information of a saved zone
     """
     user = request.user
-    # try:
-    saved_zone = SavedZone.objects.get(user=user.user_id,zone=zone)
-    zone = Zone.objects.get(id=saved_zone.zone.id)
-    dict_item={}
-    dict_item['zoneID'] = zone.id
-    dict_item['name'] = zone.name
-    dict_item['borough'] = zone.borough
-    census = zone_census_serializer(str(zone.id))
-    dict_item['median_income'] = census['median_income']
-    dict_item['mode_age_group'] = census['main_group']
-    dict_item['females_under_5'] = census['females_under_5']
-    dict_item['females_5_14'] = census['females_5_14']
-    dict_item['females_15_24'] = census['females_15_24']
-    dict_item['females_25_34'] = census['females_25_34']
-    dict_item['females_35_44'] = census['females_35_44']
-    dict_item['females_45_54'] = census['females_45_54']
-    dict_item['females_55_64'] = census['females_55_64']
-    dict_item['females_65_74'] = census['females_65_74']
-    dict_item['females_75_84'] = census['females_75_84']
-    dict_item['males_85'] = census['females_85']
-    dict_item['males_under_5'] = census['males_under_5']
-    dict_item['males_5_14'] = census['males_5_14']
-    dict_item['males_15_24'] = census['males_15_24']
-    dict_item['males_25_34'] = census['males_25_34']
-    dict_item['males_35_44'] = census['males_35_44']
-    dict_item['males_45_54'] = census['males_45_54']
-    dict_item['males_55_64'] = census['males_55_64']
-    dict_item['males_65_74'] = census['males_65_74']
-    dict_item['males_75_84'] = census['males_75_84']
-    dict_item['males_85'] = census['males_85']
-    detail = today_info(zone.id)
-    dict_item['details'] = detail
-    return Response({"status":"1","data":dict_item})
-    # except Exception as e:
-    #     return Response({"status":"2","data":str(e)})
+    try:
+        saved_zone = SavedZone.objects.get(user=user.user_id,zone=zone)
+        zone = Zone.objects.get(id=saved_zone.zone.id)
+        dict_item={}
+        dict_item['zoneID'] = zone.id
+        dict_item['name'] = zone.name
+        dict_item['borough'] = zone.borough
+        census = zone_census_serializer(str(zone.id))
+        dict_item['median_income'] = census['median_income']
+        dict_item['mode_age_group'] = census['main_group']
+        dict_item['females_under_5'] = census['females_under_5']
+        dict_item['females_5_14'] = census['females_5_14']
+        dict_item['females_15_24'] = census['females_15_24']
+        dict_item['females_25_34'] = census['females_25_34']
+        dict_item['females_35_44'] = census['females_35_44']
+        dict_item['females_45_54'] = census['females_45_54']
+        dict_item['females_55_64'] = census['females_55_64']
+        dict_item['females_65_74'] = census['females_65_74']
+        dict_item['females_75_84'] = census['females_75_84']
+        dict_item['males_85'] = census['females_85']
+        dict_item['males_under_5'] = census['males_under_5']
+        dict_item['males_5_14'] = census['males_5_14']
+        dict_item['males_15_24'] = census['males_15_24']
+        dict_item['males_25_34'] = census['males_25_34']
+        dict_item['males_35_44'] = census['males_35_44']
+        dict_item['males_45_54'] = census['males_45_54']
+        dict_item['males_55_64'] = census['males_55_64']
+        dict_item['males_65_74'] = census['males_65_74']
+        dict_item['males_75_84'] = census['males_75_84']
+        dict_item['males_85'] = census['males_85']
+        detail = today_info(zone.id)
+        dict_item['details'] = detail
+        return Response({"status":"1","data":dict_item})
+    except Exception as e:
+        return Response({"status":"2","data":str(e)})
