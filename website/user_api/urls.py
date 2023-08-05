3eb6247be0014c7162d05d029b56a49f062f9343
minhlynguyen <90552104+minhlynguyen@users.noreply.github.com>
2023-08-05 14:04:02 +0100

    Add "/" to unify url

--- website/user_api/urls.py
+++ website/user_api/urls.py
@@ -6,11 +6,11 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-	path('register', views.UserRegister.as_view(), name='register'),
+	path('register/', views.UserRegister.as_view(), name='register'),
 	# path('login', views.UserLogin.as_view(), name='login'),
-	path('login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('login/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+	path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 	# path('logout', views.UserLogout.as_view(), name='logout'),
-	path('user', views.UserView.as_view(), name='user'),
-    path('logout/blacklist', views.BlacklistTokenView.as_view(),name='blacklist')
+	path('user/', views.UserView.as_view(), name='user'),
+    path('logout/blacklist/', views.BlacklistTokenView.as_view(),name='blacklist')
 ]
