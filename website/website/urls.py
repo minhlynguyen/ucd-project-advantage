a6b61dec53484374b9469eade4f0b1d0fe52ef87
minhlynguyen <90552104+minhlynguyen@users.noreply.github.com>
2023-08-09 11:59:54 +0100

    change api route

--- website/website/urls.py
+++ website/website/urls.py
@@ -9,8 +9,6 @@ urlpatterns = [
     path('api/admin/', admin.site.urls),
     path('api/main/',include('main.urls')),
     path('api/user/',include('user_api.urls')),
-    path('api/user/save/',include('save_api.urls',namespace='save_api')),
+    path('api/save/',include('save_api.urls',namespace='save_api')),
     path('api/api-auth/', include('rest_framework.urls', namespace='rest_framework')), 
-    # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
