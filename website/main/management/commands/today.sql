ae20250c8ba338486be3d43ce2068377883540b9
minhlynguyen <90552104+minhlynguyen@users.noreply.github.com>
2023-08-10 21:23:27 +0100

    Update cron job to run daily impression

--- website/main/management/commands/today.sql
+++ website/main/management/commands/today.sql
@@ -1,21 +1,21 @@
-INSERT INTO maps.zone_today (zone_id, 
-	date_time, 
-	impression_predict,
-	entertainment_and_recreation,
-	financial_services,
-	food_and_beverage,
-	parking_and_automotive_services,
-	professional_services,
-	real_estate,
-	retail_services,
-	transportation,
-	hospital,
-	hotspots,
-	school,
-	total_business,
-	holiday
-)
-
+UPDATE maps.zone_today AS p
+SET 
+zone_id = q.zone_id,
+date_time = q.date_time,
+entertainment_and_recreation = q.entertainment_and_recreation,
+financial_services = q.financial_services,
+food_and_beverage = q.food_and_beverage,
+parking_and_automotive_services = q.parking_and_automotive_services,
+professional_services = q.professional_services, 
+real_estate = q.real_estate, 
+retail_services = q.retail_services, 
+transportation = q.transportation, 
+hospital = q.hospital, 
+hotspots = q.hotspots, 
+school = q.school, 
+total_business = q.total_business, 
+holiday = q.holiday
+FROM (
 SELECT
 	taxi_zone_id as zone_id,
 	datetime as date_time, 
@@ -35,22 +35,7 @@ SELECT
 	holiday
 FROM maps.zone_detail
 WHERE date(datetime) = CURRENT_DATE
-
-ON CONFLICT (zone_id, date_time) DO UPDATE
-
-set
-date_time = EXCLUDED.date_time,
-entertainment_and_recreation = EXCLUDED.entertainment_and_recreation,
-financial_services = EXCLUDED.financial_services,
-food_and_beverage = EXCLUDED.food_and_beverage,
-parking_and_automotive_services = EXCLUDED.parking_and_automotive_services,
-professional_services = EXCLUDED.professional_services, 
-real_estate = EXCLUDED.real_estate, 
-retail_services = EXCLUDED.retail_services, 
-transportation = EXCLUDED.transportation, 
-hospital = EXCLUDED.hospital, 
-hotspots = EXCLUDED.hotspots, 
-school = EXCLUDED.school, 
-total_business = EXCLUDED.total_business, 
-holiday = EXCLUDED.holiday
+) AS q
+WHERE p.zone_id = q.zone_id
+AND EXTRACT(HOUR FROM p.date_time) = EXTRACT(HOUR FROM q.date_time)
 ;
